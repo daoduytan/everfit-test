0a0f32340312d762b1b919e471ed4c977b1b5bdb
daoduytan <ddtan2212@gmail.com>
2020-05-31 17:39:50 +0700

    Updates

---
+++ precache-manifest.a70bc01c5cdcae0534f0b149727c6a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1a234ae9df2d9992e86822739f7c10",
+    "url": "/everfit-test/index.html"
+  },
+  {
+    "revision": "00d632931c4bb16821b5",
+    "url": "/everfit-test/static/css/main.62d31bb9.chunk.css"
+  },
+  {
+    "revision": "d55416923393ffb510ed",
+    "url": "/everfit-test/static/js/2.f7a8378b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/everfit-test/static/js/2.f7a8378b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d632931c4bb16821b5",
+    "url": "/everfit-test/static/js/main.85d8c844.chunk.js"
+  },
+  {
+    "revision": "5ab9b9e9c6d1723a3443",
+    "url": "/everfit-test/static/js/runtime-main.ddff182e.js"
+  }
+]);
